60081294fa016b848b53b59b2d529987cb415b77
Stephanie Gooch <stephanie.gooch@hotmail.com>
2021-05-13 14:56:44 +0100

    role arn

--- iam.tf
+++ iam.tf
@@ -59,7 +59,7 @@ resource "aws_iam_role_policy" "iam_role_policy_for_organisation" {
             "Sid": "assume",
             "Effect": "Allow",
             "Action": "sts:AssumeRole",
-            "Resource": "${var.management_account_id}"
+            "Resource": "arn:aws:iam::${var.management_account_id}:role/OrganizationLambdaAccessRole"
         }
     ]
 }

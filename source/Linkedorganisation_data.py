b22f92b6a75c2809a2199b5dc66a16ee5d12b754
Stephanie Gooch <stephanie.gooch@hotmail.com>
2021-05-15 15:32:58 +0100

    updated fmt

--- source/Linkedorganisation_data.py
+++ source/Linkedorganisation_data.py
@@ -22,10 +22,10 @@ def list_tags(client, resource_id):
     return tags
     
 def lambda_handler(event, context):
-
+    management_account_id = os.environ["MANAGMENT_ACCOOUNT_ID"]
     sts_connection = boto3.client('sts')
     acct_b = sts_connection.assume_role(
-        RoleArn="arn:aws:iam::(account id):role/OrganizationLambdaAccessRole",
+        RoleArn=f"arn:aws:iam::{management_account_id}:role/OrganizationLambdaAccessRole",
         RoleSessionName="cross_acct_lambda"
     )
     
@@ -77,7 +77,7 @@ def account_data(f, parent, parent_name, client):
 def s3_upload(file_name):
     bucket = os.environ["BUCKET_NAME"] #Using environment variables below the Lambda will use your S3 bucket
     try:
-        s3 = boto3.client('s3', '(Region)',
+        s3 = boto3.client('s3', os.environ["REGION"],
                         config=Config(s3={'addressing_style': 'path'}))
         s3.upload_file(
             f'/tmp/{file_name}.json', bucket, f"organisation-data/{file_name}.json") #uploading the file with the data to s3
@@ -88,35 +88,35 @@ def s3_upload(file_name):
 
 
 def get_ou_ids(parent_id, client):
-  full_result = {}
-  
-  paginator = client.get_paginator('list_organizational_units_for_parent')
-  iterator  = paginator.paginate(
+    full_result = {}
+
+    paginator = client.get_paginator('list_organizational_units_for_parent')
+    iterator  = paginator.paginate(
     ParentId=parent_id
 
-  )
+    )
 
-  for page in iterator:
+    for page in iterator:
     for ou in page['OrganizationalUnits']:
-      print(ou['Name'])
-      full_result[ou['Id']]=[]
-      full_result[ou['Id']].append(ou['Name'])
+        print(ou['Name'])
+        full_result[ou['Id']]=[]
+        full_result[ou['Id']].append(ou['Name'])
 
 
-  return full_result
+    return full_result
 
 def get_acc_ids(parent_id,  client):
-  full_result = []
-  
-  paginator = client.get_paginator('list_accounts_for_parent')
-  iterator  = paginator.paginate(
+    full_result = []
+
+    paginator = client.get_paginator('list_accounts_for_parent')
+    iterator  = paginator.paginate(
     ParentId=parent_id
-  )
+    )
 
-  for page in iterator:
+    for page in iterator:
     for acc in page['Accounts']:
-      print(acc['Id'])
-      full_result.append(acc['Id'])
+        print(acc['Id'])
+        full_result.append(acc['Id'])
 
 
-  return full_result
+    return full_result
